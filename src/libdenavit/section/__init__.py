2f78d574d6dbe61ca78fe978f0058dad8111e92c
denavit <denavit@gmail.com>
2022-06-12 19:06:49 -0400

    Add missing import

--- src/libdenavit/section/__init__.py
+++ src/libdenavit/section/__init__.py
@@ -4,6 +4,7 @@ from .wide_flange import WideFlangeDB, WideFlangeMember_AISC2016
 from .fiber_patches import FiberSingle, FiberQuadPatch, FiberCirclePatch
 from .fiber_section import FiberSection
 from .ACI_strain_compatibility import AciStrainCompatibility
+from .ACI_phi import ACI_phi
 from .geometric_shape import GeometricShape
 from .rectangle_shape import Rectangle,PlateMember_AISC2016
 from .reinf import Reinf,ReinfRect
