46b204b01178653e6499d1ddfeaf0a0581d1050c
denavit <denavit@gmail.com>
2022-06-21 01:46:03 -0400

    Adding reinforcing_bar_database

--- src/libdenavit/section/database/__init__.py
+++ src/libdenavit/section/database/__init__.py
@@ -1,1 +1,2 @@
 from .aisc import angle_database,double_angle_database,wide_flange_database
+from .aci import reinforcing_bar_database
---
+++ src/libdenavit/section/database/aci.py
@@ -0,0 +1,15 @@
+# From ACI 318-19, Appendix B
+
+reinforcing_bar_database = {
+    '#3':  {'diameter':0.375, 'area': 0.11, 'weight': 0.376, 'soft_metric_size':'#10'},
+    '#4':  {'diameter':0.500, 'area': 0.20, 'weight': 0.668, 'soft_metric_size':'#13'},
+    '#5':  {'diameter':0.625, 'area': 0.31, 'weight': 1.043, 'soft_metric_size':'#16'},
+    '#6':  {'diameter':0.750, 'area': 0.44, 'weight': 1.502, 'soft_metric_size':'#19'},
+    '#7':  {'diameter':0.875, 'area': 0.60, 'weight': 2.044, 'soft_metric_size':'#22'},
+    '#8':  {'diameter':1.000, 'area': 0.79, 'weight': 2.670, 'soft_metric_size':'#25'},
+    '#9':  {'diameter':1.128, 'area': 1.00, 'weight': 3.400, 'soft_metric_size':'#29'},
+    '#10': {'diameter':1.270, 'area': 1.27, 'weight': 4.303, 'soft_metric_size':'#32'},
+    '#11': {'diameter':1.410, 'area': 1.56, 'weight': 5.313, 'soft_metric_size':'#36'},
+    '#14': {'diameter':1.693, 'area': 2.25, 'weight': 7.65,  'soft_metric_size':'#43'},
+    '#18': {'diameter':2.257, 'area': 4.00, 'weight':13.60,  'soft_metric_size':'#57'},
+}
