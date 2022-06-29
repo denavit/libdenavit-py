1bd538fac1da9cecc5d702acc7ecea9ba0e271b4
Javad <jvd.esm@gmail.com>
2022-06-29 13:55:13 -0400

    Update aci.py
    Add #2 rebar

--- src/libdenavit/section/database/aci.py
+++ src/libdenavit/section/database/aci.py
@@ -1,6 +1,7 @@
 # From ACI 318-19, Appendix B
 
 reinforcing_bar_database = {
+    '#2':  {'diameter':0.250, 'area': 0.05, 'weight': 0.166, 'soft_metric_size':'#6' },
     '#3':  {'diameter':0.375, 'area': 0.11, 'weight': 0.376, 'soft_metric_size':'#10'},
     '#4':  {'diameter':0.500, 'area': 0.20, 'weight': 0.668, 'soft_metric_size':'#13'},
     '#5':  {'diameter':0.625, 'area': 0.31, 'weight': 1.043, 'soft_metric_size':'#16'},
@@ -11,5 +12,5 @@ reinforcing_bar_database = {
     '#10': {'diameter':1.270, 'area': 1.27, 'weight': 4.303, 'soft_metric_size':'#32'},
     '#11': {'diameter':1.410, 'area': 1.56, 'weight': 5.313, 'soft_metric_size':'#36'},
     '#14': {'diameter':1.693, 'area': 2.25, 'weight': 7.65,  'soft_metric_size':'#43'},
-    '#18': {'diameter':2.257, 'area': 4.00, 'weight':13.60,  'soft_metric_size':'#57'},
+    '#18': {'diameter':2.257, 'area': 4.00, 'weight': 13.60, 'soft_metric_size':'#57'},
 }
