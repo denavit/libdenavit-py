e0c3e7b4573038a4618da7b56798c077637f3051
denavit <denavit@gmail.com>
2022-07-07 19:54:02 -0400

    Adding Circle to __init__.py

--- src/libdenavit/section/__init__.py
+++ src/libdenavit/section/__init__.py
@@ -7,5 +7,6 @@ from .ACI_strain_compatibility import AciStrainCompatibility
 from .ACI_phi import ACI_phi
 from .geometric_shape import GeometricShape
 from .rectangle_shape import Rectangle,PlateMember_AISC2016
+from .circle_shape import Circle
 from .reinf import Reinf,ReinfRect
 from .RC import RC
