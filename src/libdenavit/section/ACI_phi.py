ba288611b62653aca6f730fb59e2a98d17d08d89
denavit <denavit@gmail.com>
2022-07-29 23:22:52 -0400

    Allow et to be a numpy array

--- src/libdenavit/section/ACI_phi.py
+++ src/libdenavit/section/ACI_phi.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def ACI_phi(transverse_reinf_type, et, ety):
     
     # Table 21.2.2, ACI 318-19 
@@ -13,16 +15,27 @@ def ACI_phi(transverse_reinf_type, et, ety):
 
     phitc = 0.9
 
-    # Tension Controlled
-    if et >= ety + 0.003:
-        phi = phitc
+    def compute_phi(et):
+        # Tension Controlled
+        if et >= ety + 0.003:
+            phi = phitc
 
-    # Compression Controlled
-    elif et <= ety:
-        phi = phicc
+        # Compression Controlled
+        elif et <= ety:
+            phi = phicc
 
-    # Transition
-    else:
-        phi = phicc + (phitc - phicc) * (et - ety) / 0.003
+        # Transition
+        else:
+            phi = phicc + (phitc - phicc) * (et - ety) / 0.003
+        
+        return phi
+
+    if isinstance(et,np.ndarray):
+        phi = np.empty_like(et)
+        for i,iet in enumerate(et):
+            phi[i] = compute_phi(iet)
+        return phi
+        
+    phi = compute_phi(et)   
 
     return phi
