e9e9caa0c2e7433387b96a314182373913004622
Javad <jvd.esm@gmail.com>
2022-08-03 17:27:19 -0400

    Create get_fiber_data.py

---
+++ src/libdenavit/OpenSees/get_fiber_data.py
@@ -0,0 +1,65 @@
+import openseespy.opensees as ops
+import json
+import os
+
+
+def get_fiber_data(section_tag: str, filename='data_fiber_0xB0101010.json', keep_json=False):
+    if os.path.exists(filename):
+        ans = input('The file already exists. Enter "Y" to overwrite it or "N" to cancel: ')
+        if ans == 'N':
+            raise Exception('Cancelled by user to prevent overwriting the json file.')
+
+    ops.printModel('-JSON', '-file', f'{filename}')
+    output_data = json.load(open(str(filename)))
+    sections_list = output_data['StructuralAnalysisModel']['properties']['sections']
+    x = []
+    y = []
+    A = []
+    m = []
+
+    for ind, value in enumerate(sections_list):
+        if value["name"] == str(section_tag):
+            section = value
+
+    for j in section['fibers']:
+        x.append(j['coord'][0])
+        y.append(j['coord'][1])
+        A.append(j['area'])
+        m.append(j['material'])
+
+    if not keep_json:
+        os.remove(f'{filename}')
+
+    return x, y, A, m
+
+
+if __name__ == "__main__":
+    # An example of how to use this function
+    ops.wipe()
+    ops.model('basic', '-ndm', 3, '-ndf', 6)
+
+    ops.node(0, 0, 0, 0)
+    ops.node(1, 0, 0, 0)
+
+    ops.fix(1, 1, 0, 1, 1, 0, 0)
+    ops.uniaxialMaterial('Elastic', 1, 1000)
+    ops.uniaxialMaterial('Elastic', 2, 2000)
+
+    ops.section("Fiber", 1, '-GJ', 10e6)
+    ops.fiber(0, 0, 5, 2)
+    ops.patch('quad', 1, 10, 10, -5, -5, 5, -5, 5, 5, -5, 5)
+    ops.element('zeroLengthSection', 1, 0, 1, 1)
+
+    ops.section("Fiber", 2, '-GJ', 10e6)
+    ops.fiber(0, 0, 5, 2)
+    ops.fiber(1, 1, 5, 2)
+    ops.fiber(-1, -1, 5, 2)
+    ops.fiber(1, -1, 5, 2)
+    ops.fiber(-1, 1, 5, 2)
+    ops.element('zeroLengthSection', 2, 0, 1, 2)
+
+    x, y, A, m = get_fiber_data("1")
+    print(f'x: {x}\n', f'y: {y}\n', f'A: {A}\n', f'm: {m}\n')
+
+    x, y, A, m = get_fiber_data("2")
+    print(f'x: {x}\n', f'y: {y}\n', f'A: {A}\n', f'm: {m}\n')
