674e0ab644a9eb6e1eda7b69311563022f24e105
denavit <denavit@gmail.com>
2022-08-03 22:40:58 -0400

    Modifying names and other clean up

--- src/libdenavit/__init__.py
+++ src/libdenavit/__init__.py
@@ -4,4 +4,5 @@ from .unit_convert import unit_conversion_factor, unit_convert
 from .camber import camber
 from .joist import OpenWebSteelJoist, JoistGirder
 from .goal_seek import GoalSeekMonotonic
+from .interaction_diagram_2d import InteractionDiagram2d
 from .non_sway_column_2d import NonSwayColumn2d
