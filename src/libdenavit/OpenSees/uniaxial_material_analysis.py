3608603ce5b40ac4e07c23e3fd234903badc4a4d
denavit <denavit@gmail.com>
2023-01-21 06:31:58 -0500

    Update uniaxial_material_analysis.py

--- src/libdenavit/OpenSees/uniaxial_material_analysis.py
+++ src/libdenavit/OpenSees/uniaxial_material_analysis.py
@@ -2,30 +2,63 @@ import openseespy.opensees as ops
 import matplotlib.pyplot as plt
 from math import ceil
 
-def uniaxial_material_analysis(definition, peak_points, rate_type = 'None', rate_value = None, matTag=1, 
+def uniaxial_material_analysis(definition, peak_points, rate_type = 'None', rate_value = None, 
+                               def_args = [], def_kwargs = {}, matTag=1, ndm = 1, ndf = 1, parallel_stiffness = 0,
                                plot_stress_strain = False, compression_positive = False):
         
     # ------------------------------
     # Build Model
     # -----------------------------        
     ops.wipe()
-    ops.model('basic', '-ndm', 1, '-ndf', 1)
+    ops.model('basic', '-ndm', ndm, '-ndf', ndf)
     
     # Define Nodes
-    ops.node(1,0.0)
-    ops.node(2,1.0)
-
+    if ndm == 1:
+        ops.node(1,0.0)
+        ops.node(2,1.0)
+    elif ndm == 2:
+        ops.node(1,0.0,0.0)
+        ops.node(2,1.0,0.0)
+    elif ndm == 3:
+        ops.node(1,0.0,0.0,0.0)
+        ops.node(2,1.0,0.0,0.0)
+    else:
+        raise ValueError('ndm should be 1, 2, or 3')
+    
     # Define Boundary Conditions
-    ops.fix(1, 1)
+    if ndf == 1:
+        ops.fix(1, 1)
+    elif ndf == 3:
+        ops.fix(1, 1, 1, 1)
+        ops.fix(2, 0, 1, 1)
+    else:
+        raise ValueError(f'Code not implemented for ndf = {ndf}')
 
-    # Define Elements
-    ops.uniaxialMaterial(*definition)
+    # Define Material
+    if isinstance(definition, list):
+        ops.uniaxialMaterial(*definition)
+    elif callable(definition):
+        definition(*def_args,**def_kwargs)
+    else:
+        raise TypeError('Material defintion is not a type that can be used')
+       
+    # Define Element
     ops.element('Truss', 1, 1, 2, 1.0, matTag)
-     
+    
+    # Add parallel stiffness (if requested)
+    if parallel_stiffness > 0:
+        ops.uniaxialMaterial('Elastic', 24325, parallel_stiffness)
+        ops.element('Truss', 2, 1, 2, 1.0, 24325)
+        
     # Define Loads
     ops.timeSeries('Linear', 854)
     ops.pattern('Plain', 979, 854)
-    ops.load(2, 1.0)
+    if ndf == 1:
+        ops.load(2, 1.0)
+    elif ndf == 3:
+        ops.load(2, 1.0, 0.0, 0.0)
+    else:
+        raise ValueError(f'Code not implemented for ndf = {ndf}')
 
     # ------------------------------
     # Build and Perform the Analysis
@@ -39,11 +72,11 @@ def uniaxial_material_analysis(definition, peak_points, rate_type = 'None', rate
     ops.analysis('Static')
     ops.analyze(1)
     if compression_positive:
-        stress = [-1*ops.getTime()]
         strain = [-1*ops.nodeDisp(2,1)]
+        stress = [-1*ops.getTime() - parallel_stiffness*strain[-1]]
     else:
-        stress = [ops.getTime()]
         strain = [ops.nodeDisp(2,1)]
+        stress = [ops.getTime() + parallel_stiffness*strain[-1]]
     
     for i in range(len(peak_points)-1):
         if rate_type == 'None':
@@ -53,7 +86,7 @@ def uniaxial_material_analysis(definition, peak_points, rate_type = 'None', rate
         elif rate_type == 'Steps':
             num_steps = rate_value
         else:
-            raise Exception('Unknown rate_type: %s' % rate_type)
+            raise ValueError('Unknown rate_type: %s' % rate_type)
             
         incr = (peak_points[i+1] - peak_points[i])/num_steps
         
@@ -61,11 +94,11 @@ def uniaxial_material_analysis(definition, peak_points, rate_type = 'None', rate
             ops.integrator('DisplacementControl', 2, 1, incr)
             ops.analyze(1)
             if compression_positive:
-                stress.append(-1*ops.getTime())
                 strain.append(-1*ops.nodeDisp(2,1))
+                stress.append(-1*ops.getTime() - parallel_stiffness*strain[-1])
             else:
-                stress.append(ops.getTime())
                 strain.append(ops.nodeDisp(2,1))
+                stress.append(ops.getTime() + parallel_stiffness*strain[-1])
             
     if plot_stress_strain:
         fig = plt.figure()
