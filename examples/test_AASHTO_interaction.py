e15a3f5273c7659a08776a324f601dc0f95bd1ff
Javad <jvd.esm@gmail.com>
2023-02-20 17:32:46 -0500

    Update test_AASHTO_interaction
    - Add error plot

--- examples/test_AASHTO_interaction.py
+++ examples/test_AASHTO_interaction.py
@@ -1,7 +1,8 @@
-from libdenavit import NonSwayColumn2d
+from libdenavit import NonSwayColumn2d, InteractionDiagram2d
 from libdenavit.section import RC, Circle, ReinfCirc
 import matplotlib.pyplot as plt
 import math
+import numpy as np
 
 # Input Properties
 D = 48
@@ -31,30 +32,42 @@ plt.xlabel("Bending Moment (kip-in.)")
 plt.ylabel("Axial Compression (kips)")
 
 # Create NonSwayColumn2d object
-col = NonSwayColumn2d(section, length, 1, 1, dxo=length / 1000)
+col = NonSwayColumn2d(section, length, 1, 1, dxo=length / 1000, axis=axis)
 
 # Define fiber section definition arguments 
 section_args = (1, "ElasticPP", "Concrete04_no_confinement", 20, 20)
-section_kwargs = {'axis':axis}
+section_kwargs = {}
 
 # Run Analyses
 num_points = 10
-P_ops, M1_ops, M2_ops = col.run_ops_interaction(section_args, section_kwargs, num_points)
-
-P_AASHTO, M1_AASHTO, M2_AASHTO  = col.run_AASHTO_interaction(axis, EI_type='a', section_factored = False, Pc_factor = 1.00)
-
-
+Opensees_results = col.run_ops_interaction(section_args, section_kwargs, num_points)
+AASHTO_results  = col.run_AASHTO_interaction(EI_type='a', section_factored = False, Pc_factor = 1.00)
 # Make Plot
 fig = plt.figure()
-plt.plot(M1_ops, P_ops, color='tab:blue',   marker='x', linestyle='--', label='$M_1$ (OpenSees)')
-plt.plot(M2_ops, P_ops, color='tab:blue',   marker='o', linestyle='-',  label='$M_2$ (OpenSees)')
-plt.plot(M_CS1, -P_CS1, color='tab:orange', linestyle='-.')
-plt.plot(M1_AASHTO, -P_AASHTO,  color='tab:orange', marker='x', linestyle='--', label='$M_1$ (AASHTO)')
-plt.plot(M2_AASHTO, -P_AASHTO,  color='tab:orange', marker='o', linestyle='-',  label='$M_2$ (AASHTO)')
+plt.plot(Opensees_results["M1"], Opensees_results["P"], color='tab:blue',   marker='x', linestyle='--', label='$M_1$ (OpenSees)')
+plt.plot(Opensees_results["M2"], Opensees_results["P"], color='tab:blue',   marker='o', linestyle='-',  label='$M_2$ (OpenSees)')
+plt.plot(AASHTO_results["M1"], AASHTO_results["P"],  color='tab:orange', marker='x', linestyle='--', label='$M_1$ (AASHTO)')
+plt.plot(AASHTO_results["M2"], AASHTO_results["P"],  color='tab:orange', marker='o', linestyle='-',  label='$M_2$ (AASHTO)')
 plt.xlabel("Bending Moment (kip-in.)")
 plt.ylabel("Axial Compression (kips)")
 plt.legend(loc='upper right')
 
+
+angles = [x + 0.1 for x in range(0, 91)]
+angles[-1] = angles[-1] - 0.2
+
+AASHTO = InteractionDiagram2d(AASHTO_results["M1"], AASHTO_results["P"], is_closed=True)
+Opensees = InteractionDiagram2d(Opensees_results["M1"], Opensees_results["P"], is_closed=True)
+e = Opensees.compare_two(AASHTO, angles, degrees=True)
+
+fig = plt.figure(figsize=(6, 4))
+plt.plot(angles, e)
+plt.axhline(y=0, color='k')
+plt.ylabel(r'$\epsilon= \frac{r_{OpenSees} - r_{AASHTO}}{r_{OpenSees}}$')
+plt.xlabel('Angle with respect to x-axis (degrees)')
+plt.tight_layout()
+plt.xlim(0, 90)
+plt.xticks(np.arange(0, 91, 10))
 
 # Show Plots
 plt.show()
