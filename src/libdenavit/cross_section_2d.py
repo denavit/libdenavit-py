9de7da2ab812ebccdf608371e27871cd91976892
Javad <jvd.esm@gmail.com>
2023-04-28 13:01:57 -0400

    Update cross_section_2d.py

--- src/libdenavit/cross_section_2d.py
+++ src/libdenavit/cross_section_2d.py
@@ -2,7 +2,7 @@ from math import pi, sin
 from libdenavit import find_limit_point_in_list, interpolate_list
 from libdenavit.OpenSees import AnalysisResults
 import openseespy.opensees as ops
-
+import numpy as np
 
 class CrossSection2d:
     def __init__(self, section, axis=None):
@@ -31,8 +31,15 @@ class CrossSection2d:
         else:
             raise ValueError(f"ops_element_type {self.ops_element_type} not recognized")
 
-    def run_ops_analysis(self, analysis_type, section_args, section_kwargs, e=0, P=0,
-                         perc_drop=0.05, num_steps_vertical=20, load_incr_factor=1e-5):
+    def run_ops_analysis(self, analysis_type, section_args, section_kwargs, e=0, P=0, num_steps_vertical=20,
+                         load_incr_factor=1e-5, disp_incr_factor=1e-7):
+
+        eigenvalue_limit = 0
+        percent_load_drop_limit = 0.05
+        concrete_strain_limit = -0.01
+        steel_strain_limit = 0.05
+        try_smaller_steps = True
+        print_limit_point = True
 
         self.build_ops_model(1, section_args, section_kwargs)
 
@@ -47,15 +54,22 @@ class CrossSection2d:
 
         # Define function to find limit point
         def find_limit_point():
-            ind, x = find_limit_point_in_list(results.lowest_eigenvalue, 0)
-            if ind is None:
-                ind, x = find_limit_point_in_list(results.maximum_concrete_compression_strain, -0.005)
-            if ind is None:
-                results.applied_axial_load_at_limit_point = None
-                results.maximum_abs_moment_at_limit_point = None
-            else:
-                results.applied_axial_load_at_limit_point = interpolate_list(results.applied_axial_load, ind, x)
-                results.maximum_abs_moment_at_limit_point = interpolate_list(results.maximum_abs_moment, ind, x)
+            if print_limit_point:
+                print(results.exit_message)
+
+            if 'Analysis Failed' in results.exit_message:
+                ind, x = find_limit_point_in_list(results.maximum_abs_moment, max(results.maximum_abs_moment))
+            elif 'Eigenvalue Limit' in results.exit_message:
+                ind,x = find_limit_point_in_list(results.lowest_eigenvalue, eigenvalue_limit)
+            elif 'Extreme Compressive Concrete Fiber Strain Limit Reached' in results.exit_message:
+                ind, x = find_limit_point_in_list(results.maximum_concrete_compression_strain, concrete_strain_limit)
+            elif 'Extreme Steel Fiber Strain Limit Reached' in results.exit_message:
+                ind, x = find_limit_point_in_list(results.maximum_steel_strain, steel_strain_limit)
+            elif 'Load Drop Limit Reached' in results.exit_message:
+                ind, x = find_limit_point_in_list(results.maximum_abs_moment, max(results.maximum_abs_moment))
+
+            results.applied_axial_load_at_limit_point = interpolate_list(results.applied_axial_load,ind,x)
+            results.maximum_abs_moment_at_limit_point = interpolate_list(results.maximum_abs_moment,ind,x)
 
         # Run analysis
         if analysis_type.lower() == 'proportional_limit_point':
@@ -65,7 +79,7 @@ class CrossSection2d:
             ops.load(2, -1, 0, e)
             ops.integrator('LoadControl', load_incr_factor)
             ops.system('SparseGeneral', '-piv')
-            ops.test('NormUnbalance', 1e-2, 10)
+            ops.test('NormUnbalance', 1e-3, 10)
             ops.numberer('Plain')
             ops.constraints('Plain')
             ops.algorithm('Newton')
@@ -90,6 +104,55 @@ class CrossSection2d:
             maximum_applied_axial_load = 0.
             while True:
                 ok = ops.analyze(1)
+                if try_smaller_steps:
+                    if ok != 0:
+                        ops.integrator('LoadControl', load_incr_factor/10)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            load_incr_factor = load_incr_factor
+                            print(f'Changed the step size to: {load_incr_factor}')
+                
+                    if ok != 0:
+                        ops.integrator('LoadControl', load_incr_factor/100)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            load_incr_factor = load_incr_factor
+                            print(f'Changed the step size to: {load_incr_factor}')
+                    
+                    if ok != 0:
+                        ops.integrator('LoadControl', load_incr_factor/1000)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            load_incr_factor = load_incr_factor/10
+                            print(f'Changed the step size to: {load_incr_factor}')
+                    
+                    if ok != 0:
+                        ops.integrator('LoadControl', load_incr_factor/10000)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            load_incr_factor = load_incr_factor/10
+                            print(f'Changed the step size to: {load_incr_factor}')
+
+                if ok != 0:
+                    print('Trying ModifiedNewton')
+                    ops.algorithm('ModifiedNewton')
+                    ok = ops.analyze(1)
+
+                if ok != 0:
+                    print('Trying KrylovNewton')
+                    ops.algorithm('KrylovNewton')
+                    ok = ops.analyze(1)
+
+                if ok != 0:
+                    print('Trying KrylovNewton and Greater Tolerance')
+                    ops.algorithm('KrylovNewton')
+                    ops.test('NormUnbalance', 1e-2, 10)
+                    ok = ops.analyze(1)
+
+                if ok == 0:
+                    ops.algorithm('Newton')
+                    ops.test('NormUnbalance', 1e-3, 10)
+
                 if ok != 0:
                     results.exit_message = 'Analysis Failed'
                     break
@@ -97,21 +160,30 @@ class CrossSection2d:
                 record()
 
                 # Check for drop in applied load
-                current_applied_axial_load = results.applied_axial_load[-1]
-                maximum_applied_axial_load = max(maximum_applied_axial_load, current_applied_axial_load)
-                if current_applied_axial_load < (1 - perc_drop) * maximum_applied_axial_load:
-                    results.exit_message = 'Load Drop Limit Reached'
-                    break
+                if percent_load_drop_limit is not None:
+                    current_applied_axial_load = results.applied_axial_load[-1]
+                    maximum_applied_axial_load = max(maximum_applied_axial_load, current_applied_axial_load)
+                    if current_applied_axial_load < (1 - percent_load_drop_limit) * maximum_applied_axial_load:
+                        results.exit_message = 'Load Drop Limit Reached'
+                        break
 
                 # Check for lowest eigenvalue less than zero
-                if results.lowest_eigenvalue[-1] < 0:
-                    results.exit_message = 'Eigenvalue Limit Reached'
-                    break
+                if eigenvalue_limit is not None:
+                    if results.lowest_eigenvalue[-1] < eigenvalue_limit:
+                        results.exit_message = 'Eigenvalue Limit Reached'
+                        break
 
-                # Check for strain in extreme compressive fiber
-                if results.maximum_concrete_compression_strain[-1] < -0.005:
-                    results.exit_message = 'Extreme Compressive Fiber Strain Limit Reached'
-                    break
+                # Check for strain in extreme compressive concrete fiber
+                if concrete_strain_limit is not None:
+                    if results.maximum_concrete_compression_strain[-1] < concrete_strain_limit:
+                        results.exit_message = 'Extreme Compressive Concrete Fiber Strain Limit Reached'
+                        break
+
+                # Check for strain in extreme steel fiber
+                if steel_strain_limit is not None:
+                    if results.maximum_steel_strain[-1] > steel_strain_limit:
+                        results.exit_message = 'Extreme Steel Fiber Strain Limit Reached'
+                        break
 
             find_limit_point()
             return results
@@ -124,12 +196,12 @@ class CrossSection2d:
             ops.constraints('Plain')
             ops.numberer('RCM')
             ops.system('UmfPack')
-            ops.test('NormUnbalance', 1, 10)
+            ops.test('NormUnbalance', 1e-3, 10)
             ops.algorithm('Newton')
             ops.integrator('LoadControl', P / num_steps_vertical)
             ops.analysis('Static')
 
-            # Define recorder
+            # region Define recorder
             def record():
                 time = ops.getTime()
                 results.applied_axial_load.append(time)
@@ -141,8 +213,10 @@ class CrossSection2d:
                     self.section.maximum_concrete_compression_strain(axial_strain, curvatureX=curvatureX))
                 results.maximum_steel_strain.append(
                     self.section.maximum_tensile_steel_strain(axial_strain, curvatureX=curvatureX))
+            # endregion
 
             record()
+
             for i in range(num_steps_vertical):
                 ok = ops.analyze(1)
 
@@ -152,18 +226,18 @@ class CrossSection2d:
 
                 record()
 
-            # endregion
+            # endregion Run vertical load (time = LFV)
 
             # Run lateral load (time = LFH)
             ops.loadConst('-time', 0.0)
-
             ops.timeSeries('Linear', 101)
             ops.pattern('Plain', 201, 101)
             ops.load(2, 0, 0, 1)
-            ops.integrator('LoadControl', load_incr_factor)
+            ops.integrator('DisplacementControl', 2, 3, disp_incr_factor)
             ops.analysis('Static')
 
-            # Define recorder
+            # region Define recorder
+
             def record():
                 time = ops.getTime()
                 results.applied_axial_load.append(P)
@@ -176,11 +250,66 @@ class CrossSection2d:
                 results.maximum_steel_strain.append(
                     self.section.maximum_tensile_steel_strain(axial_strain, curvatureX))
 
+            # endregion
+
             record()
 
             maximum_time = 0
+
             while True:
                 ok = ops.analyze(1)
+                if try_smaller_steps:
+                    if ok != 0:
+                        print(f'Trying the step size of: {disp_incr_factor/10}')
+                        ops.integrator('DisplacementControl', 1, 3, disp_incr_factor/10)
+                        ok = ops.analyze(1)
+                    
+                    if ok != 0:
+                        print(f'Trying the step size of: {disp_incr_factor/100}')
+                        ops.integrator('DisplacementControl', 1, 3, disp_incr_factor/100)
+                        ok = ops.analyze(1)
+                    
+                    if ok != 0:
+                        print(f'Trying the step size of: {disp_incr_factor/1000}')
+                        ops.integrator('DisplacementControl', 1, 3, disp_incr_factor/1000)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            disp_incr_factor = disp_incr_factor/10
+                            print(f'Changed the step size to: {disp_incr_factor}')
+                    
+                    if ok != 0:
+                        print(f'Trying the step size of: {disp_incr_factor/10000}')
+                        ops.integrator('DisplacementControl', 1, 3, disp_incr_factor/10000)
+                        ok = ops.analyze(1)
+                        if ok == 0:
+                            disp_incr_factor = disp_incr_factor/10
+                            print(f'Changed the step size to: {disp_incr_factor/10}')
+
+                if ok != 0:
+                    print('Trying ModifiedNewton')
+                    ops.algorithm('ModifiedNewton')
+                    ok = ops.analyze(1)
+                    if ok == 0:
+                        print('ModifiedNewton worked')
+
+                if ok != 0:
+                    print('Trying KrylovNewton')
+                    ops.algorithm('KrylovNewton')
+                    ok = ops.analyze(1)
+                    if ok == 0:
+                        print('KrylovNewton worked')
+
+                if ok != 0:
+                    print('Trying KrylovNewton and Greater Tolerance')
+                    ops.algorithm('KrylovNewton')
+                    ops.test('NormUnbalance', 1e-4, 10)
+                    ok = ops.analyze(1)
+                    if ok == 0:
+                        print('KrylovNewton worked')
+
+                if ok == 0:
+                    ops.algorithm('Newton')
+                    ops.test('NormUnbalance', 1e-3, 10)
 
                 if ok != 0:
                     results.exit_message = 'Analysis Failed'
@@ -189,19 +318,30 @@ class CrossSection2d:
                 record()
 
                 # Check for drop in applied load (time = the horizontal load factor)
-                current_time = ops.getTime()
-                maximum_time = max(maximum_time, current_time)
-                if current_time < (1 - perc_drop) * maximum_time:
-                    results.exit_message = 'Load Drop Limit Reached'
-                    break
+                if percent_load_drop_limit is not None:
+                    current_time = ops.getTime()
+                    maximum_time = max(maximum_time, current_time)
+                    if current_time < (1 - percent_load_drop_limit) * maximum_time:
+                        results.exit_message = 'Load Drop Limit Reached'
+                        break
 
                 # Check for lowest eigenvalue less than zero
-                if results.lowest_eigenvalue[-1] < 0:
-                    results.exit_message = 'Eigenvalue Limit Reached'
-                    break
-                if results.maximum_concrete_compression_strain[-1] < -0.005:
-                    results.exit_message = 'Extreme Compressive Fiber Strain Limit Reached'
-                    break
+                if eigenvalue_limit is not None:
+                    if results.lowest_eigenvalue[-1] < 0:
+                        results.exit_message = 'Eigenvalue Limit Reached'
+                        break
+
+                # Check for strain in extreme compressive concrete fiber
+                if concrete_strain_limit is not None:
+                    if results.maximum_concrete_compression_strain[-1] < concrete_strain_limit:
+                        results.exit_message = 'Extreme Compressive Concrete Fiber Strain Limit Reached'
+                        break
+
+                # Check for strain in extreme steel fiber
+                if steel_strain_limit is not None:
+                    if results.maximum_steel_strain[-1] > steel_strain_limit:
+                        results.exit_message = 'Extreme Steel Fiber Strain Limit Reached'
+                        break
 
             find_limit_point()
             return results
