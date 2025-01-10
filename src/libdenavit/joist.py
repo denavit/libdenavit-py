35f362b8934d92a6da3fa76afdb2d23ca08a1048
denavit <denavit@gmail.com>
2025-01-09 19:53:28 -0500

    Improve handling of units

--- src/libdenavit/joist.py
+++ src/libdenavit/joist.py
@@ -3,8 +3,8 @@ import numpy as np
 import libdenavit
 
 class BaseJoistClass:
-    def moment_strength_ratio(self,x,Mr):
-        (positive_envelope,negative_envelope) = self.moment_strength_envelope(x)
+    def moment_strength_ratio(self,x,Mr,length_units='ft',moment_units='kip-ft'):
+        (positive_envelope,negative_envelope) = self.moment_strength_envelope(x,length_units=length_units,moment_units=moment_units)
         
         strength_ratio = np.zeros_like(Mr)
         for i in range(len(Mr)):
@@ -18,8 +18,8 @@ class BaseJoistClass:
         return strength_ratio
         
         
-    def shear_strength_ratio(self,x,Vr):
-        (positive_envelope,negative_envelope) = self.shear_strength_envelope(x)
+    def shear_strength_ratio(self,x,Vr,length_units='ft',force_units='kip'):
+        (positive_envelope,negative_envelope) = self.shear_strength_envelope(x,length_units=length_units,force_units=force_units)
         
         strength_ratio = np.zeros_like(Vr)
         for i in range(len(Vr)):
@@ -47,20 +47,21 @@ class OpenWebSteelJoist(BaseJoistClass):
         self.minimum_shear_reversal_strength_ratio = 0.0
         self.use_proposed_shear_strength = True
         
-    def moment_strength_envelope(self,x,units='lb-ft'):
+    def moment_strength_envelope(self,x,length_units='ft',moment_units='lb-ft'):
         
         # NOTE: calculations are performed as if the design length equals the span. 
         L = self.span_ft
         
         # Compute envelopes
-        positive_envelope = 0.5*self.wTL_plf*x*(L-x)
+        x_ft = libdenavit.unit_convert(x,length_units,'ft')
+        positive_envelope = 0.5*self.wTL_plf*x_ft*(L-x_ft)
         negative_envelope = np.zeros_like(x)
         
         # Convert to desired units
-        ucf = libdenavit.unit_conversion_factor('lb-ft', units)               
+        ucf = libdenavit.unit_conversion_factor('lb-ft', moment_units)               
         return (ucf*positive_envelope,ucf*negative_envelope)
 
-    def shear_strength_envelope(self,x,units='lbf',spread_dist=1e-6):
+    def shear_strength_envelope(self,x,length_units='ft',force_units='lbf',spread_dist=1e-6):
         
         # Create copy of x and spread out pairs of values
         x2 = x.copy()
@@ -75,23 +76,25 @@ class OpenWebSteelJoist(BaseJoistClass):
         R = 0.5*self.wTL_plf*L # End reaction
         
         for i in range(len(x2)):
+            
+            x2i = libdenavit.unit_convert(x2[i],length_units,'ft')
         
             # Basic shear strength
-            if x2[i] <= 0.5*self.span_ft:
-                positive_envelope[i] = max(0.25*R,self.wTL_plf*(0.5*L-x2[i]))
+            if x2i <= 0.5*self.span_ft:
+                positive_envelope[i] = max(0.25*R,self.wTL_plf*(0.5*L-x2i))
                 negative_envelope[i] = -self.minimum_shear_reversal_strength_ratio*R
             else:
                 positive_envelope[i] = self.minimum_shear_reversal_strength_ratio*R
-                negative_envelope[i] = min(-0.25*R,self.wTL_plf*(0.5*L-x2[i]))           
+                negative_envelope[i] = min(-0.25*R,self.wTL_plf*(0.5*L-x2i))           
 
             if self.use_proposed_shear_strength:
                 # Use minimum shear strength provisions that are not in the 2020 SJI Specification, but
                 # that are required of all SJI members
-                positive_envelope[i] = max(positive_envelope[i],0.75*R*(1-x2[i]/L)**2 + 0.25*R*(1-2*x2[i]/L))
-                negative_envelope[i] = min(negative_envelope[i],-0.75*R*(x2[i]/L)**2 - 0.25*R*(2*x2[i]/L-1))
+                positive_envelope[i] = max(positive_envelope[i],0.75*R*(1-x2i/L)**2 + 0.25*R*(1-2*x2i/L))
+                negative_envelope[i] = min(negative_envelope[i],-0.75*R*(x2i/L)**2 - 0.25*R*(2*x2i/L-1))
 
         # Convert to desired units
-        ucf = libdenavit.unit_conversion_factor('lbf', units)
+        ucf = libdenavit.unit_conversion_factor('lbf',force_units)
         return (ucf*positive_envelope,ucf*negative_envelope)
     
     def moment_of_inertia(self):
@@ -108,7 +111,7 @@ class JoistGirder(BaseJoistClass):
         self.num_spaces = num_spaces
         self.P_kips = P_kips
         
-    def moment_strength_envelope(self,x,units='lb-ft'):
+    def moment_strength_envelope(self,x,length_units='ft',moment_units='kip-ft'):
         
         # NOTE: calculations are performed as if the design length equals the span. 
         L = self.span_ft
@@ -117,18 +120,19 @@ class JoistGirder(BaseJoistClass):
         positive_envelope = np.zeros_like(x)
         negative_envelope = np.zeros_like(x)
         for i in range(len(x)):
-            s1 = floor(self.num_spaces*x[i]/L)
+            xi = libdenavit.unit_convert(x[i],length_units,'ft')
+            s1 = floor(self.num_spaces*xi/L)
             M1 = self.P_kips*L*(self.num_spaces-s1)*s1/(2*self.num_spaces)
             M2 = self.P_kips*L*(self.num_spaces-(s1+1))*(s1+1)/(2*self.num_spaces)
             x1 = s1/self.num_spaces*L
             x2 = (s1+1)/self.num_spaces*L
-            positive_envelope[i] = M1 + (M2-M1)*(x[i]-x1)/(x2-x1)
+            positive_envelope[i] = M1 + (M2-M1)*(xi-x1)/(x2-x1)
         
         # Convert to desired units
-        ucf = libdenavit.unit_conversion_factor('kip-ft', units)             
+        ucf = libdenavit.unit_conversion_factor('kip-ft', moment_units)
         return (ucf*positive_envelope,ucf*negative_envelope)
 
-    def shear_strength_envelope(self,x,units='lbf',spread_dist=1e-6):
+    def shear_strength_envelope(self,x,length_units='ft',force_units='lbf',spread_dist=1e-6):
         
         # Create copy of x and spread out pairs of values
         x2 = x.copy()
@@ -143,13 +147,15 @@ class JoistGirder(BaseJoistClass):
         R = 0.5*self.P_kips*(self.num_spaces-1) # End reaction
         
         for i in range(len(x2)):
-            if x2[i] == L:
+            x2i = libdenavit.unit_convert(x2[i],length_units,'ft')
+        
+            if x2i == L:
                 s1 = self.num_spaces
             else:
-                s1 = floor(self.num_spaces*x2[i]/L) + 1
+                s1 = floor(self.num_spaces*x2i/L) + 1
             Vi = self.P_kips*((self.num_spaces+1)/2 - s1) 
             
-            if x2[i] <= 0.5*self.span_ft:
+            if x2i <= 0.5*self.span_ft:
                 positive_envelope[i] = max(Vi,0.25*R)
                 negative_envelope[i] = -0.25*positive_envelope[i]               
             else:
@@ -157,7 +163,7 @@ class JoistGirder(BaseJoistClass):
                 positive_envelope[i] = -0.25*negative_envelope[i]   
                 
         # Convert to desired units
-        ucf = libdenavit.unit_conversion_factor('kips', units)
+        ucf = libdenavit.unit_conversion_factor('kips', force_units)
         return (ucf*positive_envelope,ucf*negative_envelope)
    
     def moment_of_inertia(self):
