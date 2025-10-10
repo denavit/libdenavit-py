b95662024082076e5dcbbb3e0dcc80bdd1305bb7
Prajjwal <prajjwal.learning@gmail.com>
2025-10-10 12:39:49 -0400

    Updates to AISC_Member_2022

--- src/libdenavit/section/wide_flange.py
+++ src/libdenavit/section/wide_flange.py
@@ -498,17 +498,15 @@ class I_shape(GeometricShape):
                                     + self.bf/2 * abs(curvatureY)
         return extreme_strain
 
-class WideFlangeMember_AISC2023:
+class WideFlangeMember_AISC2022:
     def __init__(self,section,Fy,E,L,**kwargs):
         self.section = section
         self.Fy = Fy
         self.E = E
         self.L=L
 
-        defaults={'strength_type':'lrfd',
-                  'Lb':L,
-                  'Lcx':L,
-                  'Lcy':L}
+        defaults={'strength_type':'lrfd'
+                  }
         
         for key,value in defaults.items():
             setattr(self,key,kwargs.get(key,value))
@@ -520,62 +518,154 @@ class WideFlangeMember_AISC2023:
         if self.section.h_over_tw > 3.76*sqrt(self.E/self.Fy) :
             raise Exception('Mn not yet implemented for noncompact or slender webs')
     def check_slenderness_of_flange(self):
-        if self.section.bf_over_2tf> 0.56*sqrt(self.E/self.Fy) :
-            raise Exception('Pn not yet implemented for slender flanges')
+        limit = 0.56 * sqrt(self.E / self.Fy)
+        return self.section.bf_over_2tf > limit
     def check_slenderness_of_web(self):
-        if self.section.h_over_tw > 1.49*sqrt(self.E/self.Fy) :
-            raise Exception('Pn not yet implemented for slender webs')    
-            
-    def Mnx(self): 
+        limit = 1.49 *sqrt(self.E / self.Fy)
+        return self.section.h_over_tw > limit  
+          
+    def Ae(self,Fcr):
+        Ae = self.section.A
+    
+        # Effective flange width
+        lam = self.section.bf_over_2tf
+        lam_r = 0.56*sqrt(self.E/self.Fy)
+        if lam > lam_r*sqrt(self.Fy/Fcr):
+            c1 = 0.22
+            c2 = 1.49
+            Fel = (c2*lam_r/lam)**2*self.Fy
+            be = self.section.bf*(1-c1*sqrt(Fel/Fcr))*sqrt(Fel/Fcr)
+            Ae = Ae - 2*(self.section.bf-be)*self.section.tf
+        
+        # Effective web height
+        lam = self.section.h_over_tw
+        lam_r = 1.49*sqrt(self.E/self.Fy)
+        if lam > lam_r*sqrt(self.Fy/Fcr):
+            c1 = 0.18
+            c2 = 1.31
+            Fel = (c2*lam_r/lam)**2*self.Fy
+            h = lam*self.section.tw
+            he = h*(1-c1*sqrt(Fel/Fcr))*sqrt(Fel/Fcr)
+            Ae = Ae - (h-he)*self.section.tw
+        
+        return Ae
+
+    def Mnx(self,Lb,Cb):
         """Moment strength of member for major-axis bending.
 
-        Reference: AISC Specification Sections F1, F2
+        Parameters
+        ----------
+        Lb : float
+            Unbraced length of the member.
+        Cb : float
+            Lateral-torsional buckling modification factor.
+
+        Notes
+        -----
+        - Not yet implemented for noncompact or slender webs.
+
+        Reference: AISC Specification Chapter F; Sections F1 -- F3
         """
-        ## check_compactness_of_flange and webs
-        self.check_compactness_of_flange()
-        self.check_compactness_of_web()
-        ## Yielding
-        Mn=self.Fy*self.section.Zx
-        print('Line 527',self.Fy)
-        print('Line 529',self.section.Zx)
-        ## Lateral Torsional Buckling
-        Lp=1.76*self.section.ry*sqrt(self.E/self.Fy)
-        if self.Lb>Lp:
-            raise Exception('Mn not yet implemented for Lateral Torsional buckling with Lb>Lp')
+
+        # Check width-to-thickness ratios
+        self.check_compactness_of_web()           
+               
+        # Yielding 
+        Mp = self.Fy*self.section.Zx
+        Mn = Mp
+
+        # Compression Flange Local Buckling
+        λ  = self.section.bf_over_2tf
+        λp = 0.38*sqrt(self.E/self.Fy)
+        λr = 1.0*sqrt(self.E/self.Fy)
+        if λ <= λp:
+            pass
+        elif λ <= λr:
+            Mn_CFLB = Mp - (Mp-0.7*self.Fy*self.section.Sx)*(λ-λp)/(λr-λp)
+            Mn = min(Mn,Mn_CFLB)
+        else:
+            kc = 4/sqrt(self.section.h_over_tw)
+            if kc > 0.76:
+                kc = 0.76
+            if kc < 0.35:
+                kc = 0.35
+            Mn_CFLB = (0.9*self.E*kc*self.section.Sx)/(λ**2)
+            Mn = min(Mn,Mn_CFLB)
+
+        # Lateral-Torsional Buckling
+        Lp = 1.76*self.section.ry*sqrt(self.E/self.Fy)
+        Lr = 1.95*self.section.rts*(self.E/(0.7*self.Fy))*sqrt((self.section.J/(self.section.Sx*self.section.ho))+sqrt(((self.section.J/(self.section.Sx*self.section.ho))**2)+(6.76*((0.7*self.Fy)/self.E)**2)))
         
+        if Lb <= Lp:
+            pass
+        if Lb <= Lr:
+            Mn_LTB = Cb*((self.Fy*self.section.Zx)-(((self.Fy*self.section.Zx)-(0.7*self.Fy*self.section.Sx))*((Lb-Lp)/(Lr-Lp))))
+            Mn = min(Mn,Mn_LTB)
+        else:
+            Fcr= Cb*(pi**2)*((self.E)/((Lb/self.section.rts)**2))*sqrt(1+(0.078*((self.section.J/(self.section.Sx*self.section.ho))*((Lb/self.section.rts)**2))))
+            Mn_LTB = Fcr*self.section.Sx
+            Mn = min(Mn,Mn_LTB)
+                
         return available_strength(Mn,self.strength_type,0.9,1.67)
 
-    def Mny(self): 
+    def Mny(self):
         """Moment strength of member for minor-axis bending.
 
         Reference: AISC Specification Sections F1, F6
         """
-        ## Yielding
-        Mn=min(self.Fy*self.section.Zy,1.6*self.Fy*self.section.Sy)
-        ## Flange local Buckling
-        ## check_compactness_of_flange and webs
-        self.check_compactness_of_flange()
+        # Yielding
+        Mp = min(self.Fy*self.section.Zy, 1.6*self.Fy*self.section.Sy)
+        Mn = Mp
+
+        # Flange local buckling
+        λ  = self.section.bf_over_2tf
+        λp = 0.38*sqrt(self.E/self.Fy)
+        λr = 1.0*sqrt(self.E/self.Fy)
+
+        if λ <= λp:
+            pass
+        elif λ <= λr:
+            Mn_CFLB = Mp - (Mp-0.7*self.Fy*self.section.Sy)*(λ-λp)/(λr-λp)
+            Mn = min(Mn,Mn_CFLB)
+        else:
+            Fcr = 0.7*self.E/λ**2
+            Mn_CFLB = Fcr*self.section.Sy
+            Mn = min(Mn,Mn_CFLB)
         
         return available_strength(Mn,self.strength_type,0.9,1.67)
 
-    def Pn(self,bending_axes):
-        """Nominal compressive strength.
-
-        Reference: AISC Specification Sections E1, E3
-        """   
-        self.check_slenderness_of_flange()
-        self.check_slenderness_of_web()
-        r=self.section.rx if bending_axes=='x' else self.section.ry
-        Lc=self.Lcx if bending_axes=='x' else self.Lcy
-        Fe=((pi**2)*self.E)/(Lc/r)**2
-        var=self.Fy/Fe
-        if var<=2.25:
-            Fn=(0.658**var)*self.Fy
+    def Pnc(self,Lcx,Lcy):
+        # Does not compute torsional buckling
+        
+        # Major-axis flexural buckling
+        if Lcx == 0.0:
+            Fcrx = self.Fy
         else:
-            Fn=0.877*Fe
-
-        Pn=Fn*self.section.A
-
+            Fe = pi**2*self.E/(Lcx/self.section.rx)**2
+            if self.Fy/Fe <= 2.25:
+                Fcrx = 0.658**(self.Fy/Fe)*self.Fy
+            else:
+                Fcrx = 0.877*Fe            
+        
+        # Minor-axis flexural buckling
+        if Lcy == 0.0:
+            Fcry = self.Fy
+        else:
+            Fe = pi**2*self.E/(Lcy/self.section.ry)**2
+            if self.Fy/Fe <= 2.25:
+                Fcry = 0.658**(self.Fy/Fe)*self.Fy
+            else:
+                Fcry = 0.877*Fe  
+                
+        # Compute strength
+        Fcr = min(Fcrx,Fcry)
+        slender=self.check_slenderness_of_flange() or self.check_slenderness_of_web()
+        if not slender:
+            A=self.section.A
+        else:
+            A=self.Ae(Fcr)
+        Pn = Fcr*A
+        
         return available_strength(Pn,self.strength_type,0.9,1.67)
 
 
@@ -730,7 +820,7 @@ class WideFlangeMember_AISC2016:
             Mn_CFLB = Mp - (Mp-0.7*self.Fy*self.section.Sy)*(λ-λp)/(λr-λp)
             Mn = min(Mn,Mn_CFLB)
         else:
-            Fcr = 0.69*self.E/λ**2
+            Fcr = 0.69*self.E/λ**2 ## Has this changed to 0.7 in AISC 2022
             Mn_CFLB = Fcr*self.section.Sy
             Mn = min(Mn,Mn_CFLB)
         
