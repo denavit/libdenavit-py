703ae9138c711d18926998fbea0336f113bcec51
Mark D. Denavit <denavit@gmail.com>
2026-02-15 19:08:17 -0500

    Adding WideFlangeMember_AISC2022

--- src/libdenavit/section/__init__.py
+++ src/libdenavit/section/__init__.py
@@ -1,7 +1,7 @@
 from .geometric_shape import GeometricShape
 from .angle import Angle
 from .double_angle import DoubleAngle, DoubleAngleMember_SJI2020
-from .wide_flange import WideFlangeDB, I_shape, WideFlangeMember_AISC2016
+from .wide_flange import WideFlangeDB, I_shape, WideFlangeMember_AISC2016, WideFlangeMember_AISC2022
 from .fiber_patches import FiberSingle, FiberQuadPatch, FiberCirclePatch
 from .fiber_section import FiberSection
 from .ACI_strain_compatibility import AciStrainCompatibility
